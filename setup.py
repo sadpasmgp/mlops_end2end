600d88ff78d8eb5168b5ca29f856ebcc9b58e90e
sadpasmgp <sadpasmgp@outlook.com>
2022-04-14 00:53:50 +0530

    mlops first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='mlops implementation with wafer prediction project',
+    author='sadpasmgp',
+    license='',
+)
